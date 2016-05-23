46bcb0a4e15ff3a03211e71db1d379a844a0ca98
Phils Garcia <eivanphils@gmail.com>
2016-05-23 16:06:37 -0430

    Pag 131 - realizando la consulta en el repositorio

--- src/Cupon/OfertaBundle/Entity/OfertaRepository.php
+++ src/Cupon/OfertaBundle/Entity/OfertaRepository.php
@@ -7,6 +7,18 @@ class OfertaRepository extends EntityRepository
 {
     public function findOfertaDelDia($ciudad)
     {
-        
+        $em = $this->getEntityManager();
+
+        $dql = 'SELECT o, c, t
+                FROM OfertaBundle:Oferta o
+                JOIN o.ciudad c JOIN o.tienda t
+                WHERE o.revisada = TRUE 
+                AND c.slug = :ciudad';
+
+        $consulta = $em->createQuery($dql);
+        $consulta->setParameter('ciudad', $ciudad);
+        $consulta->setMaxResults(1);
+
+        return $consulta;
     }
 }
