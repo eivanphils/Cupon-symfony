995950b58ea2080abbafe5ba5ecc0953ea1737f2
Phils Garcia <eivanphils@gmail.com>
2016-06-07 14:26:32 -0430

    Pag 157 - Agregar lista desplegable de ciudades, y cambio de contenido por la ciudad seleccionada

--- src/Cupon/CiudadBundle/Controller/DefaultController.php
+++ src/Cupon/CiudadBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Cupon\CiudadBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
@@ -13,5 +14,25 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('CiudadBundle:Default:index.html.twig');
+    }
+
+//    /**
+//     * @Route("/ciudad/cambiar-a-{ciudad}")
+//     */
+    public function cambiarAction($ciudad){
+        return new RedirectResponse($this->generateUrl('portada',
+            array(
+                'ciudad' => $ciudad)
+        ));
+    }
+
+    public function listaCiudadesAction(){
+        $em = $this->getDoctrine()->getManager();
+        $ciudades = $em->getRepository('CiudadBundle:Ciudad')->findAll();
+
+        return $this->render('CiudadBundle:Default:listaCiudades.html.twig',
+            array(
+                'ciudades' => $ciudades)
+        );
     }
 }
---
+++ src/Cupon/CiudadBundle/Twig/Extension/listaCiudades.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Cupon\CiudadBundle\Twig\Extension;
+
+class listaCiudades extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'lista_ciudades';
+    }
+}
