2127db3aa3ee94cf5ccecff668c8ec7b25e011d0
Phils Garcia <eivanphils@gmail.com>
2016-06-10 16:27:58 -0430

    Pag 162 - mostrando las ofertas de otras ciudades

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -46,11 +46,15 @@ class DefaultController extends Controller
     public function ofertaAction($ciudad, $slug)
     {
         $em = $this->getDoctrine()->getManager();
+
         $oferta = $em->getRepository('OfertaBundle:Oferta')->findOferta($ciudad, $slug);
+
+        $relacionadas = $em->getRepository('OfertaBundle:Oferta')->findRelacionadas($ciudad);
 
         return $this->render('OfertaBundle:Default:detalle.html.twig',
             array(
-                'oferta' => $oferta
+                'oferta' => $oferta,
+                'relacionadas' => $relacionadas
             ));
     }
 }
--- src/Cupon/OfertaBundle/Entity/OfertaRepository.php
+++ src/Cupon/OfertaBundle/Entity/OfertaRepository.php
@@ -21,23 +21,41 @@ class OfertaRepository extends EntityRepository
 
         return $consulta->getSingleResult();
     }
+
     public function findOferta($ciudad, $slug)
     {
         $em = $this->getEntityManager();
 
-        $query = 'SELECT o, c, t 
-                  from OfertaBundle:Oferta o 
+        $dql = 'SELECT o, c, t 
+                  FROM OfertaBundle:Oferta o 
                   JOIN o.ciudad c JOIN o.tienda t 
                   WHERE o.revisada = TRUE 
                   AND o.slug = :slug
                   AND c.slug = :ciudad';
 
-        $consulta = $em->createQuery($query);
+        $consulta = $em->createQuery($dql);
         $consulta->setParameter('slug', $slug);
         $consulta->setParameter('ciudad', $ciudad);
         $consulta->setMaxResults(1);
 
         return $consulta->getSingleResult();
+    }
 
+    public function findRelacionadas($ciudad)
+    {
+        $em = $this->getEntityManager();
+
+        $dql = 'SELECT o,c 
+                  FROM OfertaBundle:Oferta o 
+                  JOIN o.ciudad c 
+                  WHERE o.revisada = TRUE 
+                  AND c.slug != :ciudad 
+                  ORDER BY o.nombre';
+
+        $consulta = $em->createQuery($dql);
+        $consulta->setMaxResults(5);
+        $consulta->setParameter('ciudad', $ciudad);
+
+        return $consulta->getResult();
     }
 }
