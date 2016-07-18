b4687b5f6e4cd8de30922aee14a58d71f600d473
Phils Garcia <eivanphils@gmail.com>
2016-07-17 22:31:02 -0430

    Config send simple email

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+
 class DefaultController extends Controller
 {
     /**
@@ -17,5 +18,34 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+
+//    /**
+//     * @Route("redirection", name="redirect_email")
+//     */
+//    public function redirectionEmailAction()
+//    {
+//
+//    }
+
+    /**
+     * @Route("/sendemail/{name}", name="send_simple_email")
+     */
+    public function sendEmailAction($name)
+    {
+        $message = \Swift_Message::newInstance()
+            ->setSubject('New account | Barcode Workshop')
+            ->setFrom('not-reply@barcodeworkshop.com')
+            ->setTo('eivanphils@gmail.com')
+            ->setBody(
+                $this->renderView(
+                // app/Resources/views/Emails/registration.html.twig
+                    'email/send.html.twig',
+                    array('name' => $name)
+                ),
+                'text/html'
+            );
+        $this->get('mailer')->send($message);
+                return $this->render(':email:index.html.twig');
     }
 }
